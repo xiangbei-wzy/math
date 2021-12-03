be337980ddef1766e104b756613c1451554d88ed
xiangbei-wzy <404893503@qq.com>
2021-12-03 23:37:44 +0800

    feat: update func see
    Signed-off-by: xiangbei-wzy <404893503@qq.com>

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xiangbei-wzy/math/see"
+)
+
+func main() {
+	see.Atest()
+	fmt.Println(see.Getbvar())
+}
--- see/see.go
+++ see/see.go
@@ -2,10 +2,16 @@ package see
 
 import "fmt"
 
+var bvar = "jsdhfjsdhfg sdkjgf klsdhf"
+
 func Atest() {
 	fmt.Println("A test")
 }
 
 func btest() {
 	fmt.Println("b test")
+}
+
+func Getbvar() string {
+	return bvar
 }
